048f10eba0c4abc9fb168cc95a07f346d266762e
Andrei Prakapovich <andre1121311@gmail.com>
2021-08-01 19:56:10 +0200

    Correct index.html

---
+++ js-file/firebase.config.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQS0rAMD0TQU1OixSkOBdGFqPN56IsG6c",
+    authDomain: "task-add-list.firebaseapp.com",
+    projectId: "task-add-list",
+    storageBucket: "task-add-list.appspot.com",
+    messagingSenderId: "3121757557",
+    appId: "1:3121757557:web:3e010f5deb045cd6b3273c"
+};
