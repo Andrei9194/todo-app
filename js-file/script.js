dbbbf1178f9432f2fb892377f55a06dffd45e835
Andrei Prakapovich <andre1121311@gmail.com>
2021-08-01 21:37:50 +0200

    Add sing In function

--- js-file/script.js
+++ js-file/script.js
@@ -1,37 +1,58 @@
 firebase.initializeApp(firebaseConfig);
-
 const db = firebase.firestore();
 const auth = firebase.auth();
 
 const signUpButton = document.querySelector('.signup')
 const signInButton = document.querySelector('.signin')
-const registForm = document.querySelector('#registration-form')
+const registForm = document.querySelector('#signup-form')
+const mainForm = document.querySelector('.mainForm')
 
+const getData = (e) => {
+    e.preventDefault();
 
-signUpButton.addEventListener('click', (e) => {
+    const email = registForm['email'].value;
+    const password = registForm['password'].value;
+
+    const datas = {
+        email,
+        password
+    }
+    if (registForm.id === 'signup-form') {
+        userSignUp(datas)
+    } else {
+        userSignIn(datas)
+    }
+    registForm.reset();
+}
+signUpButton.addEventListener('click', () => {
     registForm.style.backgroundColor = "red"
-    registForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const userNickName = registForm['nickname'].value;
-        const email = registForm['email'].value
-        const password = registForm['password'].value
-
-        auth.createUserWithEmailAndPassword(email, password)
-            .then((token) => {
-                return db.collection('users').doc(token.user.uid).set({
-                    username: userNickName
-                })
-            })
-            .then(() => {
-                registForm.reset();
-            })
-            .then(() => {
-                auth.signOut().then(() => {
-                    console.log('sign out')
-                })
-            })
-
-    })
-
+    registForm.id = 'signup-form'
 })
+signInButton.addEventListener('click', () => {
+    registForm.style.backgroundColor = "blue"
+    registForm.id = 'sign-form'
+})
+
+const userSignIn = (token) => {
+    auth.signInWithEmailAndPassword(token.email, token.password).then(() => {
+        mainForm.classList.add('hidden')
+    })
+}
+
+const userSignUp = (token) => {
+    const userNickName = registForm['nickname'].value
+
+    auth.createUserWithEmailAndPassword(token.email, token.password)
+        .then((cred) => {
+            return db.collection('users').doc(cred.user.uid).set({
+                username: userNickName
+            })
+        })
+        .then(() => {
+            auth.signOut().then(() => {
+                console.log('sign out')
+            })
+        })
+}
+
+registForm.addEventListener('submit', getData);
